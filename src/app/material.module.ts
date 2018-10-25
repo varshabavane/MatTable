b71026b2602cdc7d56ab60e22dfc1822ce2e2fa8
varsha bavane <varshab13bavane@gmail.com>
2018-10-25 13:29:07 +0530

    gridlistmodule $ inpute module added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,32 @@
 import{NgModule} from '@angular/core';
+import{MatTableModule} from '@angular/material/table';
+//import{MatHeaderCell} from '@angular/material/MatHeaderCell';
+import {MatGridListModule} from '@angular/material/grid-list';
+//import {MatGridTileModule} from '@angular/material/grid-tile';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { MatInputModule } from '@angular/material/input';
+
+
+
+
 
 @NgModule({   
-    imports: [],
-    exports: []
+    imports: [MatTableModule,
+        MatGridListModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatInputModule,
+        //MatGridTileModule
+    ],
+    exports: [MatTableModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+       // MatGridTileModule,
+        MatGridListModule,
+        MatInputModule
+    ]
 }) 
  
 export class MaterialModule {}
